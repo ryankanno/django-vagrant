6aa599162b4499a2cf45ffc21e77d0c3721eecbf
Ryan Kanno <ryankanno@localkinegrinds.com>
2012-07-01 07:54:21 -0400

    Added python/user dependencies Created django user Squashed commit of the following:
    commit 69b6295a13a7308063ff4e3305206f37b977f7fd
    Author: Ryan Kanno <ryankanno@localkinegrinds.com>
    Date:   Sun Jul 1 07:48:47 2012 -0400
    
        Added stuffs
    
    commit 1ca46f2d47340de02619407d209bc9e62769f54c
    Author: Ryan Kanno <ryankanno@localkinegrinds.com>
    Date:   Sat Jun 30 07:51:51 2012 -0400
    
        Added python dependencies
        Added user deps

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,60 @@
 node default {
-    class {'nginx': }
-    class {'mysql::python': }
+    class { "users": stage => "pre" }
+    class { "python": stage => "py" }
+    class { "application": }
+}
+
+stage { "py": before => Stage["main"] }
+class python {
+    package {
+        "python-dev": ensure => "2.7.3-0ubuntu2";
+        "python": ensure => "2.7.3-0ubuntu2";
+        "python-setuptools": ensure => installed;
+    }
+}
+
+stage { "pre": before => Stage["py"] }
+class users {
+
+    package { 
+        'build-essential': ensure => latest;
+        'libshadow': ensure => latest, provider => 'gem', require => Package["build-essential"];
+    }
+    
+    group { "app":
+        ensure => present,
+        gid => 2000 
+    }
+
+    user { "django":
+        ensure => present,
+        gid => "app",
+        groups => ["adm", "root"],
+        password => '$6$QgrFgGzO$jrmISp8T9vIcKq.DumvXVJcAvcEth7lmLY1EcG.ljkbc3KpU3NEwH8TkQzs4rmUJvsBw5zcUNpWGU4.X84AwB/',
+        shell => "/bin/bash",
+        require => [Group["app"], Package["libshadow"]]
+    }
+
+    file { "/var/www":
+        ensure => "directory",
+        owner  => "vagrant",
+        group  => "app",
+        mode   => 755,
+        require => Group["app"]
+    }
+
+    file { "/var/www/apps":
+        ensure => "directory",
+        owner  => "django",
+        group  => "app",
+        mode   => 755,
+        require => [ Group["app"], User["django"] ] 
+    }
+}
+
+class application {
+    class { 'nginx': }
+    class { 'mysql::python': }
     class { 'mysql::server':
         config_hash => { 'root_password' => '' }
     }
