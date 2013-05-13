afbb729df485664138bd95b2fe9742772be9e896
James Cleveland <jc@blackflags.co.uk>
2013-05-13 16:11:41 +0100

    cleaning output

--- data/init.sh
+++ data/init.sh
@@ -5,4 +5,4 @@ echo Generated SSH key. Printing out PUBLIC key to paste into GitHub/Codebase:
 echo ========== COPY BETWEEN THESE LINES =========
 cat ~/.ssh/id_rsa.pub
 echo ========== COPY BETWEEN THESE LINES =========
-read -p "Once you have done this, press enter."
+read -p "Once you have done this, press enter... "
