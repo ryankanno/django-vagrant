df90a1a895491550a8eb1a6f32c36c811bae505d
James Cleveland <jc@blackflags.co.uk>
2013-05-13 16:09:02 +0100

    Added init script2

---
+++ data/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo Generating SSH key - you should be ok to hit enter for most of these options.
+ssh-keygen
+echo Generated SSH key. Printing out PUBLIC key to paste into GitHub/Codebase:
+echo ========== COPY BETWEEN THESE LINES =========
+cat ~/.ssh/id_rsa.pub
+echo ========== COPY BETWEEN THESE LINES =========
+read -p "Once you have done this, press enter."
